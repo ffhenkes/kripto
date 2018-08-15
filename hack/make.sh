2636da998db56504d6ec4a0ba8bcceae6cbcfb5d
Deathstroke <fabio.favero@gmail.com>
2018-08-15 11:44:35 -0300

    [FIX] Moving .files.

--- hack/make.sh
+++ hack/make.sh
@@ -8,11 +8,11 @@ SRC=src/$REPOSITORY/$GROUP/$PROJECT/
 rm -rf $GOPATH/$SRC/
 echo 'removed: '$GOPATH/$SRC
 
-shopt -s extglob
+shopt -s extglob dotglob
 
 mkdir -p $GOPATH/$SRC && mv !(hack*) $GOPATH/$SRC
 
-shopt -u extglob
+shopt -u extglob dotglob
 
 cd $GOPATH/$SRC
 
